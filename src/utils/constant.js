69d0dac687b469bef602ab3978e993033b0bfcbc
jolyeh <yehezekocolojoelprince@gmail.com>
2025-11-07 16:32:47 +0100

    railway deploy

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,2 @@
-export const urlApi = "https://api-salon-six.vercel.app";
-//export const urlApi = "https://api-librairie.vercel.app/api";
+export const urlApi = "https://api-salon-production-2bcd.up.railway.app";
+//export const urlApi = "http://0.0.0.0:3000";
